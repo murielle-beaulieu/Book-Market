717030e27bc1b39dd561e30c0825ea186a5f6bcf
Murielle Beaulieu <muriellebeaulieu@Murielles-MacBook-Pro.local>
2025-08-07 15:49:59 +1000

    filtering by author and book genre

--- book-market/src/main/java/mb/projects/book_market/Book/BookController.java
+++ book-market/src/main/java/mb/projects/book_market/Book/BookController.java
@@ -31,22 +31,39 @@ public class BookController {
     @AllowedRoles({ Role.ADMIN })
     @GetMapping("/all")
     public ResponseEntity<List<Book>> getAllBooks() {
-        List<Book> all = this.bookServices.getAllBooks();
-        return new ResponseEntity<>(all , HttpStatus.OK);
+        List<Book> all = bookServices.getAllBooks();
+        return new ResponseEntity<>(all, HttpStatus.OK);
+    }
+
+    @AllowedRoles({ Role.ADMIN })
+    @GetMapping("/availability")
+    public ResponseEntity<List<Book>> getBooksByAvailability(@RequestParam String status) throws Exception {
+        List<Book> booksByAvailability = new ArrayList<>();
+        booksByAvailability = bookServices.getBooksByAvailability(status);
+        return new ResponseEntity<>(booksByAvailability, HttpStatus.OK);
     }
 
     @GetMapping()
-    public ResponseEntity<List<Book>> getBooksByAvailability(@RequestParam (required = false) String status) {
-        List<Book> booksByAvailability = new ArrayList<>();
-        if (!status.isBlank()) {
-            booksByAvailability  = this.bookServices.getBooksByAvailability(status);
-        }
-        return new ResponseEntity<>(booksByAvailability , HttpStatus.OK);
+    public ResponseEntity<List<Book>> getAvailableBooks() {
+        List<Book> availableBooks = bookServices.getAllAvailableBooks();
+        return new ResponseEntity<>(availableBooks, HttpStatus.OK);
+    }
+
+    @GetMapping("/author")
+    public ResponseEntity<List<Book>> getBooksByAuthor(@RequestParam String author) {
+    List<Book> booksByAuthor = bookServices.getBookByAuthor(author);
+    return new ResponseEntity<>(booksByAuthor, HttpStatus.OK);
+    }
+
+    @GetMapping("/genre")
+    public ResponseEntity<List<Book>> getBooksByGenre(@RequestParam String genre) {
+    List<Book> booksByGenre = bookServices.getBookByGenre(genre);
+    return new ResponseEntity<>(booksByGenre, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Book> getBookById(@PathVariable Long id) {
-        Book found = this.bookServices.getBookById(id);
+        Book found = bookServices.getBookById(id);
         return new ResponseEntity<>(found, HttpStatus.OK);
     }
 
@@ -58,18 +75,18 @@ public class BookController {
 
     @PatchMapping("/{id}")
     public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody UpdateBookDTO data) {
-        Book updatedBook = this.bookServices.updateBook(id,data);
+        Book updatedBook = bookServices.updateBook(id, data);
         return new ResponseEntity<>(updatedBook, HttpStatus.OK);
     }
 
     @PatchMapping("/changeAvailability/{id}")
     public void changeAvailability(@PathVariable Long id) {
-        this.bookServices.changeAvailability(id);
+        bookServices.changeAvailability(id);
     }
 
     @DeleteMapping("/{id}")
     public String deleteBook(@PathVariable Long id) {
-        this.bookServices.deleteBook(id);
+        bookServices.deleteBook(id);
         return new String("Successfully deleted book with ID: " + id);
     }
 
--- book-market/src/main/java/mb/projects/book_market/Book/BookService.java
+++ book-market/src/main/java/mb/projects/book_market/Book/BookService.java
@@ -1,6 +1,7 @@
 package mb.projects.book_market.Book;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -8,6 +9,7 @@ import java.util.stream.Collectors;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import mb.projects.book_market.Enums.BookGenre;
 import mb.projects.book_market.User.User;
 import mb.projects.book_market.User.UserRepository;
 
@@ -24,11 +26,15 @@ public class BookService {
         this.mapper = mapper;
     }
 
+    public List<Book> getAllBooks() {
+        return this.bookRepo.findAll();
+    }
+
     public List<Book> getBooksByAvailability(String filter) {
-        
+
         List<Book> filtered = new ArrayList<>();
 
-        if (filter.equalsIgnoreCase("available")){
+        if (filter.equalsIgnoreCase("available")) {
             filtered = getAllAvailableBooks();
         }
 
@@ -37,10 +43,6 @@ public class BookService {
         }
 
         return filtered;
-    }
-
-    public List<Book> getAllBooks() {
-        return this.bookRepo.findAll();
     }
 
     public List<Book> getAllAvailableBooks() {
@@ -63,9 +65,26 @@ public class BookService {
         return result;
     }
 
+    public List<Book> getBookByAuthor(String author) {
+        List<Book> all = getAllBooks();
+        return all.stream().filter(book -> book.getAuthor().equals(author)).collect(Collectors.toList());
+    }
+
+    public List<Book> getBookByGenre(String genre) {
+        List<BookGenre> allBookGenres = Arrays.stream(BookGenre.values()).collect(Collectors.toList());
+        BookGenre selectedGenre = allBookGenres.stream().filter(g -> g.toString().equals(genre)).findFirst().get();
+
+        List<Book> all = getAllBooks();
+        List<Book> byGenre = all.stream().filter(book -> book.getBookGenres().contains(selectedGenre))
+                .collect(Collectors.toList());
+
+        return byGenre;
+    }
+
     public Book createBook(BookDTO data) throws Exception {
         Book newBook = mapper.map(data, Book.class);
-        User owner = userRepo.findById(data.getUser_id()).orElseThrow(() -> new Exception("No User matching id: " + data.getUser_id()));
+        User owner = userRepo.findById(data.getUser_id())
+                .orElseThrow(() -> new Exception("No User matching id: " + data.getUser_id()));
         newBook.setUser(owner);
         bookRepo.save(newBook);
         return newBook;
@@ -74,7 +93,7 @@ public class BookService {
     public void tradeBook(Book book, Long id) {
         // We find the book we want to copy
         Book bookTraded = bookRepo.findById(book.getId()).get();
-        
+
         // We create a new book instance and map to a new instance
         Book newBook = new Book();
         newBook = mapper.map(bookTraded, Book.class);
@@ -88,13 +107,13 @@ public class BookService {
         newBook.setUser(userReceivingBook);
 
         System.out.println("the new book owner: " + newBook.getUser().getId());
-        
+
         // We mark the book copied as having been traded
         bookTraded.setOfferedInTrade((Boolean.TRUE));
-        
+
         // We mark the new book that it's been received from a trade
         newBook.setReceivedFromTrade((Boolean.TRUE));
-        
+
         // We save both the new instance and copied instance
         bookRepo.save(newBook);
         bookRepo.save(bookTraded);
@@ -110,13 +129,14 @@ public class BookService {
     public void deleteBook(Long id) {
         Book book = getBookById(id);
         book.setIsDeleted(Boolean.TRUE);
+        book.setIsAvailable(Boolean.FALSE);
         bookRepo.save(book);
     }
 
     public void changeAvailability(Long id) {
-       Book book = getBookById(id);
-       book.setIsAvailable(!book.getIsAvailable());
-       bookRepo.save(book);
+        Book book = getBookById(id);
+        book.setIsAvailable(!book.getIsAvailable());
+        bookRepo.save(book);
     }
 
 }
