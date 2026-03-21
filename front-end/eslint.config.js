1dcb6c301ae10eb47ceaad9dfd677c76c29860c8
Murielle Beaulieu <muriellebeaulieu@Murielles-MacBook-Pro.local>
2026-03-21 11:21:18 +1100

    deleted previous frontend to start afresh

--- front-end/eslint.config.js
+++ front-end/eslint.config.js
@@ -1,84 +1,23 @@
-import js from "@eslint/js"
-import vitestPlugin from "@vitest/eslint-plugin"
-import prettierConfig from "eslint-config-prettier/flat"
-import reactPlugin from "eslint-plugin-react"
-import reactHooksPlugin from "eslint-plugin-react-hooks"
-import globals from "globals"
-import { config, configs } from "typescript-eslint"
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-const eslintConfig = config(
+export default defineConfig([
+  globalIgnores(['dist']),
   {
-    name: "global-ignores",
-    ignores: [
-      "**/*.snap",
-      "**/dist/",
-      "**/.yalc/",
-      "**/build/",
-      "**/temp/",
-      "**/.temp/",
-      "**/.tmp/",
-      "**/.yarn/",
-      "**/coverage/",
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
     ],
-  },
-  {
-    name: `${js.meta.name}/recommended`,
-    ...js.configs.recommended,
-  },
-  configs.strictTypeChecked,
-  configs.stylisticTypeChecked,
-  vitestPlugin.configs.recommended,
-  {
-    name: "eslint-plugin-react/jsx-runtime",
-    ...reactPlugin.configs.flat["jsx-runtime"],
-  },
-  reactHooksPlugin.configs["recommended-latest"],
-  {
-    name: "main",
-    linterOptions: {
-      reportUnusedDisableDirectives: 2,
-    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-    settings: {
-      vitest: {
-        typecheck: true,
-      },
-    },
-    rules: {
-      "no-undef": [0],
-      "@typescript-eslint/consistent-type-definitions": [2, "type"],
-      "@typescript-eslint/consistent-type-imports": [
-        2,
-        {
-          prefer: "type-imports",
-          fixStyle: "separate-type-imports",
-          disallowTypeAnnotations: true,
-        },
-      ],
-      "no-restricted-imports": [
-        2,
-        {
-          paths: [
-            {
-              name: "react-redux",
-              importNames: ["useSelector", "useStore", "useDispatch"],
-              message:
-                "Please use pre-typed versions from `src/app/hooks.ts` instead.",
-            },
-          ],
-        },
-      ],
     },
   },
-
-  prettierConfig,
-)
-
-export default eslintConfig
+])
--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,24 +1,10 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-import { Provider } from "react-redux"
-import "./index.css"
-import { store } from "../src/app/store";
-import App from "./App";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-const container = document.getElementById("root")
-
-if (container) {
-  const root = createRoot(container)
-
-  root.render(
-    <StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </StrictMode>,
-  )
-} else {
-  throw new Error(
-    "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
-  )
-}
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
